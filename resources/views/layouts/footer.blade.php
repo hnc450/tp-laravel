40832894e8f6a3687bb40d2ed411d50dec59153c
henock <henoctumonakiese@gmail.com>
2026-05-30 15:14:56 +0100

    refactor(views): Refactor views

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,8 +1,8 @@
-    <footer>
-        <span>© 2026 Le Blog. Tous droits réservés.</span>
-        <div>
-            <a href="#">Mentions légales</a>
-            <a href="#">Confidentialité</a>
-            <a href="{{ route('dashboard.index') }}">Admin</a>
-        </div>
-    </footer>
+<footer>
+    <span>© 2026 Le Blog. Tous droits réservés.</span>
+    <div>
+        <a href="#">Mentions légales</a>
+        <a href="#">Confidentialité</a>
+        <a href="{{ route('dashboard.index') }}">Admin</a>
+    </div>
+</footer>
--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,10 +1,10 @@
-    <nav>
-        <a href="{{ route('home') }}" class="nav-logo">Le Blog</a>
-        <ul class="nav-links">
-            <li><a href="{{ route('home') }}" class="active">Accueil</a></li>
-            <li><a href="{{ route('articles.index') }}">Articles</a></li>
-            <li><a href="{{ route('categories.index') }}">Catégories</a></li>
-            <li><a href="{{ route('about') }}">À propos</a></li>
-            <li><a href="{{ route('dashboard.index') }}">Dashboard</a></li>
-        </ul>
-    </nav>
+<nav>
+    <a href="{{ route('home') }}" class="nav-logo">Le Blog</a>
+    <ul class="nav-links">
+        <li><a href="{{ route('home') }}" class="active">Accueil</a></li>
+        <li><a href="{{ route('articles.index') }}">Articles</a></li>
+        <li><a href="{{ route('categories.index') }}">Catégories</a></li>
+        <li><a href="{{ route('about') }}">À propos</a></li>
+        <li><a href="{{ route('dashboard.index') }}">Dashboard</a></li>
+    </ul>
+</nav>
